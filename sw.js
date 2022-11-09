7fadde9e35caed4404286f3c9e7608030440064e
Stephen Wilcox <stephenawilcox@gmail.com>
2022-11-08 20:10:02 -0800

    fixed serviceWorker

--- sw.js
+++ sw.js
@@ -45,12 +45,16 @@ self.addEventListener('fetch', function (event) {
   // B8. TODO - If the request is in the cache, return with the cached version.
   //            Otherwise fetch the resource, add it to the cache, and return
   //            network response.
-    event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+  
+  event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+    return cache.match(event.request).then((cachedResponse) => {
+      if(cachedResponse){
+        return cachedResponse;
+      }
       return fetch(event.request).then((fetchedResponse) => {
         cache.put(event.request, fetchedResponse.clone());
         return fetchedResponse;
-      }).catch(() => {
-        return cache.match(event.request);
       });
-    }));
+    });
+  }));
 });
